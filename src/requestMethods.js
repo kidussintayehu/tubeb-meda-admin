74ca67543043ef7d4fc353fcca2b7de007602bd9
kidussintayehu <kidussintayehu60@gmail.com>
2022-04-22 14:34:33 +0300

    fix user not found error

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "https://tibebmeda.herokuapp.com/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user || "{}")?.currentUser?.accessToken;
+
 // console.log(TOKEN);
 // const TOKEN = true
 export const publicRequest = axios.create({
